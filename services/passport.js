4b73f9fb633044a2c1b41ec03b11af4c960b6fc9
juma-vincent <vinsoncode@gmail.com>
2020-11-07 22:37:27 +0300

    added session cookies

--- services/passport.js
+++ services/passport.js
@@ -5,6 +5,17 @@ const keys = require('../config/keys');
 
 const User = mongoose.model('users');
 
+passport.serializeUser((user, done)=>{
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done)=>{
+    User.findById(id)
+    .then((user)=>{
+        done(null, user);
+    })
+});
+
 passport.use( new GoogleStrategy({
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
@@ -15,9 +26,11 @@ passport.use( new GoogleStrategy({
     .then((existingUser)=>{
      if(existingUser){
       //We already have a record with the given profile ID
+      done(null, existingUser);
      }else{
          //We dont have a user record with this ID, make a new record!
-        new User({googleId: profile.id }).save();
+        new User({googleId: profile.id }).save()
+        .then(user=> done(null, user));
      }
     })
     
