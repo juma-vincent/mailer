45e67919281684444b35ba9f6a30e6245d7c494f
juma-vincent <vinsoncode@gmail.com>
2020-11-08 14:07:41 +0300

    tweaked google strategy settings, completed the auth flow

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,8 @@
-module.exports = {
-googleClientID: '1047495901018-qh8cim98f6roc2oiedg7e17v3hcicsvg.apps.googleusercontent.com',
-googleClientSecret: 'mty_vLFuNIHlG758gbRKNI81',
-mongoURI: 'mongodb+srv://juma:0841Vince@mailer-dev.bbpat.mongodb.net/mailer-dev?retryWrites=true&w=majority',
-cookieKey: 'kehdsdldiriohspoelkhfbolkjajknaejuejskskgheygff',
-};
+if(process.env.NODE_ENV === 'production'){
+    //We are in production - return the production set of keys  
+    module.exports = require('./prod');
+}else{
+    //We are in development - return the development keys
+    module.exports = require('./dev');
+}
 
