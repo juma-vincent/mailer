02d46cbc901e93d5f12dfdf5e8f08e9ca2a61e37
juma-vincent <vinsoncode@gmail.com>
2020-11-08 13:53:46 +0300

    finished auth flow

---
+++ config/keys.js
@@ -0,0 +1,7 @@
+module.exports = {
+googleClientID: '1047495901018-qh8cim98f6roc2oiedg7e17v3hcicsvg.apps.googleusercontent.com',
+googleClientSecret: 'mty_vLFuNIHlG758gbRKNI81',
+mongoURI: 'mongodb+srv://juma:0841Vince@mailer-dev.bbpat.mongodb.net/mailer-dev?retryWrites=true&w=majority',
+cookieKey: 'kehdsdldiriohspoelkhfbolkjajknaejuejskskgheygff',
+};
+
