74e48f0a19a341dcfa150b16a4dc098f48d7b036
juma-vincent <vinsoncode@gmail.com>
2020-11-11 19:58:37 +0300

    deleted the boilerplate files which came with create-react-app, set up redux

---
+++ client/src/components/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Header from './Header';
+
+const App = ()=>{
+    return(
+        <div>
+            <BrowserRouter>
+            <div>
+                <Header/>
+            </div>
+            </BrowserRouter>
+        </div>
+    );
+
+};
+
+export default App;
