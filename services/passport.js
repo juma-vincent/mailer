c4c2248a5bfd54e7f14799f3954fa832aee96b8c
juma-vincent <vinsoncode@gmail.com>
2020-11-11 12:30:54 +0300

    refactored code in the callback within the passport.js

--- services/passport.js
+++ services/passport.js
@@ -21,19 +21,20 @@ passport.use( new GoogleStrategy({
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback',
     proxy: true,
-},  (accessToken, refreshToken, profile, done)=>{
+}, async (accessToken, refreshToken, profile, done)=>{
 
-    User.findOne({ googleId: profile.id})
-    .then((existingUser)=>{
-     if(existingUser){
-      //We already have a record with the given profile ID
-      done(null, existingUser);
-     }else{
-         //We dont have a user record with this ID, make a new record!
-        new User({googleId: profile.id }).save()
-        .then(user=> done(null, user));
-     }
-    })
+    const existingUser = await User.findOne({ googleId: profile.id})
+    
+    if(existingUser){
+    //We already have a record with the given profile ID
+    return done(null, existingUser);
+    }
+    
+    //We dont have a user record with this ID, make a new record!
+    const user= await new User({googleId: profile.id }).save()
+    done(null, user);
+     
+    
     
 })
 );
